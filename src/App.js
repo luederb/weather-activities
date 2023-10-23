6c97cc680732625fc22e851ff2cb5d8fe4be761b
Goekhan Agkurt <g.agkurt@hotmail.de>
2023-10-23 12:42:46 +0200

    create form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
-// import ActvityForm from './components/ActvityForm/ActivityForm.js';
+import { Fragment } from "react";
+import "./App.css";
+import ActvityForm from "./components/ActvityForm/ActivityForm.js";
 
 function App() {
   return (
-   //<ActvityForm />
-   <h1>"Hallo Welt!"</h1>
+    <Fragment>
+      <ActvityForm />
+    </Fragment>
   );
 }
 
--- src/components/ActvityForm/ActivityForm.js
+++ src/components/ActvityForm/ActivityForm.js
@@ -1,8 +1,27 @@
 export default function ActvityForm() {
-    return (
-    <>
-    <h1>Hallo Welt!</h1>
-    </>
-    )
+  return (
+    <section>
+      <form>
+        <h1>Add new activity:</h1>
+        <div>
+          <label htmlFor="name">Name: </label>
+
+          <input
+            id="name"
+            type="text"
+            name="name"
+            placeholder="enter your activity"
+          ></input>
+        </div>
+
+        <div>
+          <label htmlFor="good-weather">Good-weather activity: </label>
+
+          <input type="checkbox" name="good-weather" id="good-weather"></input>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </section>
+  );
 }
- console.log("hallo welt!")
+console.log("hallo welt!");
